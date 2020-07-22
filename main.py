9cca89484d8bf764bdd82068722003e65b391d4b
DBC201 <denizbcakiroglu@gmail.com>
2020-07-22 15:14:18 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+class Layer:
+    def __init__(self, sequence, operation=None, previous=None):
+        self.sequence = sequence
+        self.operation = operation
+        self.previous = previous
+
+    def guess_value(self, value, operation):
+        if operation == "addition":
+            return self.return_last_value() + value
+        elif operation == "multiplication":
+            return self.return_last_value() * value
+        else:
+            raise RuntimeError("Unknown layer type: {}".format(operation))
+
+    def return_last_value(self):
+        return self.sequence[len(self.sequence)-1]
+
+    def __str__(self):
+        return str(self.sequence)
+
+
+def same_elements(numbers): # checks if a given list contains the same elements
+    prev = numbers[0]
+    for i in numbers:
+        if prev != i:
+            return False
+    return True
+
+
+def return_layer(sequence, op): # returns a bottom layer
+    layer = []
+    for i in range(0, len(sequence)-1):
+        e = 0
+        if op == "addition":
+            e = sequence[i+1] - sequence[i]
+        elif op == "multiplication":
+            if sequence[i] == 0:  # look into this
+                e = 0
+            else:
+                e = sequence[i+1] / sequence[i]
+        else:
+            raise RuntimeError("Unknown layer type: {}".format(op))
+        layer.append(e)
+    return layer
+
+
+def populate_bottom_layers(layers, sequence, operation='', previous_layer=None): # populates layers parameter with bottom layers
+    layer_object = Layer(sequence, operation, previous_layer)
+    if len(sequence) == 1 or same_elements(sequence):
+        layers.append(layer_object)
+        return
+    populate_bottom_layers(layers, return_layer(
+        sequence, "addition"), "addition", layer_object)
+    populate_bottom_layers(layers, return_layer(
+        sequence, "multiplication"), "multiplication", layer_object)
+
+
+def return_possibilities(bottom_layers, file = None):  # prints all possibilities and returns a list of them
+    poss_numbers = []
+    for layer in bottom_layers:
+        value = layer.return_last_value()
+        while layer.previous != None:
+            value = layer.previous.guess_value(value, layer.operation)
+            layer = layer.previous
+        output = ' '.join(map(str, layer.sequence))
+        output += ' -> ' + str(value)
+        if file:
+            file.write(output + '\n')
+        else:
+            print(output)
+        poss_numbers.append(value)
+    return poss_numbers
+
+
+if __name__ == "__main__":
+    numbers = []
+    file_name = input("Enter file name:")
+    seperator = input("Enter seperator:")
+    save_file = input("Enter a file name to save results, press enter to skip:")
+    if seperator == '\\n':
+        seperator = f'\n'
+    with open(file_name, 'r') as file:
+        raw_file = file.read().strip()
+        if seperator:
+            numbers = list(map(int, raw_file.split(seperator)))
+        else:
+            numbers = list(map(int, raw_file))
+    bottom_layers = []
+    populate_bottom_layers(bottom_layers, numbers)
+    if save_file:
+        with open(save_file, 'w') as file:
+            file.write("All Calculated Possibilities\n") 
+            possible_numbers = return_possibilities(bottom_layers, file)
+    else:
+        print("All Calculated Possibilities")
+        possible_numbers = return_possibilities(bottom_layers)
---
+++ readme.txt
@@ -0,0 +1,25 @@
+How the Program Works
+
+An example sequence: 3 9 27 81
+
+The sequence is divided into two layers that represent addition and multiplication
+
+First addition layer: 6, 18, 54
+First multiplication layer: 3, 3, 3
+
+Second addition layer that derives from first addition: 12, 36
+Second Multiplication layer that derives from first addition: 3, 3
+
+Third addition layer deriving from second addition that dv from first addition: 24
+Third multiplication layer deriving from second addition that dv from first addition: 3
+
+If all the elements are the same(and there is more than one element) 
+there is no need to calculate bottom layers because constant change has been reached and the
+next rate of change must be the same.
+
+If only one element is left, a sequence can be based upon that element assuming it
+as the next rate of change.
+
+See diagram1.png for visual explanation.
+
+This was a result of a thought I had 2 years ago, and I have came up with this purely on my own. Deniz Cakiroglu 7/21/2020
